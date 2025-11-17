4137a892cc7272b20cdcca4123a305fa33d89014
41371232H <41371232h@gapps.ntnu.edu.tw>
2025-11-17 16:44:24 +0800

    background

--- src/pages/WeatherBoardPage.js
+++ src/pages/WeatherBoardPage.js
@@ -3,7 +3,20 @@ import React from 'react';
 import BoardTemplate from '../components/BoardTemplate';
 
 const WeatherBoardPage = () => {
-  return <BoardTemplate boardName="天氣" />;
+    return (
+    <div
+      style={{
+        minHeight: '100vh',
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/weather.jpg)`,
+        backgroundSize: 'cover',        // 讓圖片覆蓋整個區域
+        backgroundPosition: 'center',   // 圖片置中
+        backgroundRepeat: 'no-repeat',  // 避免重複
+        padding: '20px'                 // 內邊距
+      }}
+    >
+      <BoardTemplate boardName="天氣" />
+    </div>
+  );
 };
 
 export default WeatherBoardPage;
